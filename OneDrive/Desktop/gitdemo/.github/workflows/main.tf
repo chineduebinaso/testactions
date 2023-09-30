9db27376f259ca82231075aecb83e8e55b8a3937
chineduebinaso <chineduebinaso@yahoo.com>
2023-09-30 22:59:30 +0100

    secret1

--- OneDrive/Desktop/gitdemo/.github/workflows/main.tf
+++ OneDrive/Desktop/gitdemo/.github/workflows/main.tf
@@ -1,9 +1,11 @@
-provider "aws_vpc" test {}
+provider "aws_vpc" test {
+    region = "us-east-1"
+}
 
 resource "aws_vpc" "test" {
     cidr_block = "10.0.0.0/16"
   
-  tags= {
-    Name = Action-tes
+  #tags= {
+    #Name = Action-tes
   }
 }
