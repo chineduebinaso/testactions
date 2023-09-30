a0a4e312e7af40ad63b0cb08856dc26517bbc0cf
chineduebinaso <chineduebinaso@yahoo.com>
2023-09-30 22:24:53 +0100

    mod

--- OneDrive/Desktop/gitdemo/.github/workflows/main.tf
+++ OneDrive/Desktop/gitdemo/.github/workflows/main.tf
@@ -1,3 +1,9 @@
-provider "aws_vpc" test {
-  cidr = "10.0.0.0/16"
+provider "aws_vpc" test {}
+
+resource "aws_vpc" "test" {
+    cidr_block = "10.0.0.0/16"
+  
+  tags= {
+    Name = Action-tes
+  }
 }
